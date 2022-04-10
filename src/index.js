cc819ca1d97868ddbc21da41c3af115074c4e464
Khairul Aqram <khairulaqram21@gmail.com>
2022-04-10 21:49:41 +0700

    menambahkan file codingan webclock

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import "./style/style.css";
+import $ from "jquery";
+import moment from "moment";
+
+const displayTime = () => {
+    moment.locale("id");
+    $(".time").text(moment().format("LTS"));
+    $(".date").text(moment().format("LL"));
+};
+
+const updateTime = () => {
+    displayTime();
+    setTimeout(updateTime, 1000)
+};
+
+updateTime();
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require("path");
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "bundle.js"
+    },
+    mode: "production",
+    module: {
+        rules: [{
+            test: /\.css$/,
+            use: [{
+                    loader: "style-loader"
+                },
+                {
+                    loader: "css-loader"
+                }
+            ]
+        }]
+    }
+}
